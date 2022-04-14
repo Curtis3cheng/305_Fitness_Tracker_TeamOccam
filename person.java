cf516d90363f4f895c97fb689b3a95758fab1122
Griffin <gpalmeri@sandiego.edu>
2022-04-14 11:27:42 -0700

    variables for person

---
+++ person.java
@@ -0,0 +1,10 @@
+// superclass
+
+public class person
+{
+    private String name;
+    private float height;
+    private String gender;
+    private float age;
+    private Object[] workoutHistory;
+}
